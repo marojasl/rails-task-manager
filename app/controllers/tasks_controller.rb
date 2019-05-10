9cbba65c2072f03f910ef6cbb4caf8b46d12c5a9
Maria Antonia Rojas <marojaslondono@gmail.com>
2019-05-10 15:27:26 +0100

    Finished exercise with CRUD tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,10 +2,46 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
-    # @task = Task.find(params[:id])
   end
 
   def show
     @task = Task.find(params[:id])
+    if @task[:completed] == true
+      @status = "This task is complete"
+    else
+      @status = "This task is not complete yet"
+    end
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(tasks_params)
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(tasks_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
